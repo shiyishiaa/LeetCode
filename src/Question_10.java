5ca5788b1c4b0aa70bf8d63ca14536cf35252c7a
Administrator <a723819210@gmail.com>
2020-10-15 15:37:03 +0800

    Give up Question_10.

--- src/Question_10.java
+++ src/Question_10.java
@@ -1,175 +1,184 @@
 import java.util.ArrayDeque;
 
 public class Question_10 {
-    private static boolean priorTo(String before, String after) {
-        if (before.length() != after.length()) return before.length() < after.length();
-        if (before.length() == 1) return !before.equals(".") && after.equals(".");
-        else return !before.startsWith(".") && after.startsWith(".");
-    }
+    private static final boolean SUCCESS = false;
 
-    public static boolean isMatch(String s, String p) {
+    public static boolean fail_isMatch(String s, String p) {
         if (p == null) return s == null;
         if (s == null) return false;
-        if (p.equals(".*")) return true;
+        final String allMatch = ".*";
+        if (p.equals(allMatch)) return true;
         if (!(p.contains(".") || p.contains("*"))) return s.equals(p);
 
         var sQueue = new ArrayDeque<Character>();
         for (var k : s.toCharArray()) sQueue.add(k);
 
-        var pQueue = new ArrayDeque<Character>();
-        for (var k : p.toCharArray()) pQueue.add(k);
+        var pQueue = new ArrayDeque<String>();
+        char[] pChars = p.toCharArray();
+        for (int i = 0; i < pChars.length; i++) {
+            if (i + 1 < pChars.length) { // End condition.
+                if (pChars[i + 1] == '*') { // Pattern "?*".
+                    if (pChars[i] == '*') // Pattern "**"
+                        return false;
+                    else { //Pattern "m*" or ".*"
+                        // If before pattern is ".*", this one is covered.
+                        var pPeekLast = pQueue.peekLast();
+                        if (allMatch.equals(pPeekLast)) {
+                            i++;
+                            continue;
+                        }
 
-        // Match trailing single characters.
-        var peekLast = pQueue.peekLast();
-        if (peekLast == null) return false;
-        while (!peekLast.equals('*')) {
-            var pollLast = pQueue.pollLast();
-            var sPollLast = sQueue.pollLast();
-            if (sPollLast == null || pollLast == null) return false;
-            if (!pollLast.equals(sPollLast) && !pollLast.equals('.')) return false;
-            peekLast = pQueue.peekLast();
-            if (peekLast == null)
-                return sQueue.isEmpty();
-        }
+                        // If this pattern is ".*", all before continuous ".*" will be covered.
+                        var thisPattern = String.valueOf(pChars[i]) + pChars[i + 1];
+                        if (allMatch.equals(thisPattern)) {
+                            var lastPattern = pQueue.peekLast();
+                            while (lastPattern != null && lastPattern.endsWith("*")) {
+                                pQueue.pollLast();
+                                lastPattern = pQueue.peekLast();
+                            }
+                            pQueue.add(thisPattern);
+                            i++;
+                            continue;
+                        }
 
-        var pStack = new ArrayDeque<String>();
-
-        // Match patterns with priority.
-        while (!pQueue.isEmpty() || !sQueue.isEmpty()) {
-            if (pQueue.isEmpty()) {
-                var stackBottom = pStack.peekLast();
-                if (stackBottom != null && stackBottom.equals(".*")) return true;
-                var toMatch = pStack.pop().charAt(0);
-                var peek = sQueue.peek();
-                while (peek != null && peek.equals(toMatch)) peek = sQueue.poll();
-                return sQueue.isEmpty() && peek == null;
-            }
-
-            if (sQueue.isEmpty()) {
-                int starPattern = 0;
-                var peek = pQueue.peek();
-                while (!pQueue.isEmpty()) {
-                    if (peek != null)
-                        if (peek.equals('*')) starPattern--;
-                        else starPattern++;
-                    pQueue.poll();
-                    peek = pQueue.peek();
-                }
-                return starPattern == 0;
-            }
-
-            var nowPoll = pQueue.poll();
-            assert nowPoll != null;
-            // Single star return false.
-            if (nowPoll.equals('*')) return false;
-            var nowPeek = pQueue.peek();
-            // Trailing single characters have been matched at the start of this function.
-            assert nowPeek != null;
-
-            // Pattern is "?*";
-            if (nowPeek.equals('*')) {
-                // Patterns like "?*" will be matched according to priority. (".*"<"m*")
-                var nowPattern = new StringBuilder();
-                nowPattern.append(nowPoll).append(pQueue.poll());
-                // Store nowPattern if pattern stack is empty.
-                if (pStack.isEmpty()) {
-                    pStack.push(nowPattern.toString());
+                        // If before pattern is equivalent to this pattern, this pattern is covered.
+                        if (thisPattern.equals(pPeekLast)) {
+                            i++;
+                            continue;
+                        }
+                        pQueue.add(thisPattern);
+                        i++;
+                    }
                     continue;
                 }
-
-                var peekStack = pStack.peek();
-                // If before is "m*", remove all the same characters ("m") at the start of sQueue.
-                if (!peekStack.startsWith(".")) {
-                    pStack.pop();
-                    var peekQueue = sQueue.peek();
-                    while (peekQueue != null && peekQueue.equals(peekStack.charAt(0))) {
-                        sQueue.poll();
-                        peekQueue = sQueue.peek();
-                    }
-                }
-                // Push now pattern.
-                pStack.push(nowPattern.toString());
-                // If before is ".*", nowPattern is covered, for ".*.*m*" equals ".*".
-            } else {
-                if (nowPoll.equals('.')) // Whatever before is, "." will also be instantly matched.
-                    sQueue.poll();
-                else { // Single character like "m" will be matched instantly due to their highest priority.
-                    if (pStack.isEmpty())
-                        if (!nowPoll.equals(sQueue.poll()))
-                            return false;
-                        else
-                            continue;
-
-                    var peekStack = pStack.peek();
-                    if (peekStack.startsWith(".")) {
-                        // If before is ".*", remove all different characters first.
-                        var peek = sQueue.peek();
-                        while (peek != null && !peek.equals(nowPoll)) {
-                            sQueue.poll();
-                            peek = sQueue.peek();
-                        }
-
-                        // Calculate the min length of nowPoll character.
-                        int minLength = 1;
-                        var pSameChar = pQueue.peek();
-                        while (pSameChar != null && (pSameChar.equals('*') || pSameChar.equals(nowPoll))) {
-                            if (pSameChar.equals('*')) minLength--;
-                            else minLength++;
-                            pQueue.poll();
-                            pSameChar = pQueue.peek();
-                        }
-
-                        // Calculate the real length of nowPoll character.
-                        int realLength = 0;
-                        var sSameChar = sQueue.peek();
-                        while (sSameChar != null && sSameChar.equals(nowPoll)) {
-                            realLength++;
-                            sQueue.poll();
-                            sSameChar = sQueue.peek();
-                        }
-                        if (realLength < minLength) return false;
-
-                        pStack.pop();
-                        if (sQueue.isEmpty()) return false;
-                    } else {
-                        var toMatch = pStack.pop().charAt(0);
-                        if (nowPoll.equals(toMatch)) {
-                            int minLength = 1;
-                            var pSameChar = pQueue.peek();
-                            while (pSameChar != null && (pSameChar.equals('*') || pSameChar.equals(nowPoll))) {
-                                if (pSameChar.equals('*')) minLength--;
-                                else minLength++;
-                                pQueue.poll();
-                                pSameChar = pQueue.peek();
-                            }
-
-                            int realLength = 0;
-                            var sSameChar = sQueue.peek();
-                            while (sSameChar != null && sSameChar.equals(toMatch)) {
-                                realLength++;
-                                sQueue.poll();
-                                sSameChar = sQueue.peek();
-                            }
-
-                            if (realLength < minLength) return false;
-                        } else { // If nowPoll is not equivalent to "m".
-                            var sPeek = sQueue.peek();
-                            while (sPeek != null && sPeek.equals(toMatch)) {
-                                sQueue.poll();
-                                sPeek = sQueue.peek();
-                            }
-                            var nextDifferentChar = sQueue.poll();
-                            if (!nowPoll.equals(nextDifferentChar)) return false;
-                        }
-                    }
-                }
             }
+            // Single character like "m" or "."
+            pQueue.add(String.valueOf(pChars[i]));
         }
-        return true;
+
+        // Match the start until encountering ".*"
+        var peek = pQueue.peek();
+        while (peek != null && !peek.equals(allMatch)) {
+            char toMatch = peek.charAt(0);
+
+            if (toMatch == '.') sQueue.poll();
+
+            int minLength = 0;
+            while (peek != null && peek.charAt(0) == toMatch) {
+                minLength++;
+                if (peek.endsWith("*")) minLength--;
+                pQueue.poll();
+                peek = pQueue.peek();
+            }
+
+            int realLength = 0;
+            Character sPeek = sQueue.peek();
+            while (sPeek != null && sPeek.equals(toMatch)) {
+                realLength++;
+                sQueue.poll();
+                sPeek = sQueue.peek();
+            }
+
+            if (realLength < minLength) return false;
+        }
+
+        // Match the end until encountering ".*"
+        var peekLast = pQueue.peekLast();
+        while (peekLast != null && !peekLast.equals(allMatch)) {
+            char toMatch = peekLast.charAt(0);
+
+            if (toMatch == '.') sQueue.pollLast();
+
+            int minLength = 0;
+            while (peekLast != null && peekLast.charAt(0) == toMatch) {
+                minLength++;
+                if (peekLast.endsWith("*")) minLength--;
+                pQueue.pollLast();
+                peekLast = pQueue.peekLast();
+            }
+
+            int realLength = 0;
+            var sPeekLast = sQueue.peekLast();
+            while (sPeekLast != null && sPeekLast.equals(toMatch)) {
+                realLength++;
+                sQueue.pollLast();
+                sPeekLast = sQueue.peekLast();
+            }
+
+            if (realLength < minLength) return false;
+        }
+
+        if (allMatch.equals(pQueue.peekLast()) && allMatch.equals(pQueue.peekLast())) {
+            // Remove the start and trail. (".*")
+            pQueue.poll();
+            pQueue.pollLast();
+
+            var patterns = new ArrayDeque<String>();
+            var sb = new StringBuilder();
+
+            while (!pQueue.isEmpty()) {
+                var pPeek = pQueue.peek();
+                do {
+                    sb.append(pPeek);
+                    pQueue.poll();
+                    pPeek = pQueue.peek();
+                } while (!(allMatch.equals(peek) || pPeek == null));
+                patterns.add(sb.toString());
+                sb = new StringBuilder();
+
+                if (allMatch.equals(pPeek)) pQueue.poll();
+                else break;
+            }
+
+            if (pQueue.isEmpty()) return true;
+
+        }
+
+        if (pQueue.size() <= 1) {
+            if (pQueue.isEmpty() && sQueue.isEmpty()) return true;
+            for (var restPattern : pQueue) if (!restPattern.equals(allMatch)) return false;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(isMatch("cbaacacaaccbaabcb",
-                "c*b*b*.*ac*.*bc*a*"));
+        System.out.println(Solution_10.isMatch("aa",
+                ".*"));
+    }
+}
+
+class Solution_10 {
+    public static boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    if (j >= 2)
+                        f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+    public static boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
     }
 }
