67cef5f552ab03f353b9c93367ccb6471cc5912f
Administrator <a723819210@gmail.com>
2020-10-19 21:01:15 +0800

    Extract ListNode out.

---
+++ src/ListNode.java
@@ -0,0 +1,37 @@
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public static ListNode of(int... values) {
+        ListNode head = new ListNode();
+        ListNode pointer = head;
+        for (int i = 0; i < values.length; i++) {
+            pointer.val = values[i];
+            if (i != values.length - 1) pointer = pointer.next = new ListNode();
+        }
+        return head;
+    }
+
+    @Override
+    public String toString() {
+        var sb = new StringBuilder();
+        ListNode pointer = this;
+        for (var i = 0; pointer != null; i++) {
+            sb.append("\t[").append(i).append("]:\t").append(pointer.val).append("\n");
+            pointer = pointer.next;
+        }
+        return sb.toString();
+    }
+}
--- src/Question_2.java
+++ src/Question_2.java
@@ -32,24 +32,6 @@ public class Question_2 {
     }
 }
 
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 class Solution_2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = null, tail = null;
